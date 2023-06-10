23b1afb56bc2009246613a45f09d9a5ea5d28270
Sonny <yy2627@nyu.edu>
2023-06-10 16:37:52 +0800

    0610

--- intro/script.js
+++ intro/script.js
@@ -3,6 +3,9 @@ console.log("running")
 let inputbox = document.getElementById("textInput")
 let sentence = document.getElementById("sentence")
 let button = document.getElementById("clearStc")
+let chinese = document.getElementById("Chinese")
+let english = document.getElementById("English")
+
 
 // inputbox.addEventListener("input", function(){
 //   console.log("now")
@@ -89,6 +92,19 @@ function startScript(data){
     sentence.innerHTML = "";
   });
 
-
-
 }
+
+let intro = document.getElementById("introduction");
+let subtitle = document.getElementById("subtitle");
+
+chinese.addEventListener("click", function() {
+  console.log("CHINESE")
+  subtitle.innerHTML="看人工智能如何诠释手写输入法里的中文文字"
+  intro.innerHTML="在手机上书写中文的最常用输入方式之一是使用手指进行手写输入。<span style='font-size:25px; font-style:italic; font-weight:bold;'>《What Do (AI) See》</span>是在收集这种手写输入与人工智能的预测输出的背景下进行的一系列作品。这一系列作品包括将这种新形式的字符应用于实体书籍中，以及在电子设备中引入基于这种新形式字符的词库。该项目建立在一个数据库的基础上，编纂了通过人工智能识别成现有中文字符的包含不规则笔画组合的新型中文字符。它旨在探索一种由人与机器协作共同创造的全新书写方式。"
+});
+
+english.addEventListener("click", function() {
+  console.log("ENGLISH")
+  subtitle.innerHTML="How AI Perceives Chinese Characters That Uses Handwriting Input Methods";
+  intro.innerHTML="One of the most common input methods for writing Chinese on a phone is to use a finger to handwrite the characters. <span style='font-size:25px; font-style:italic; font-weight:bold;'>What Do (AI) See</span> is a series of works conducted in the context of a collection of this handwritten input alongside AI’s predictive output, which includes the implementation of this new form of characters in physical books and the introduction of a new thesaurus in electronic devices. This project is based on a database, compiling stroke compositions that the AI recognizes as existing Chinese characters, despite the irregularity of stroke combinations. It sheds light on a new form of writing that is being invented by a human-machine collaboration."
+});
